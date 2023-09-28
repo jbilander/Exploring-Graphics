6ffea281e768e5b18beae3beb6e0245189666bcb
jbilander <jorgen.bilander@gmail.com>
2023-09-28 12:23:46 +0200

    Update file

--- RTL/main_top.v
+++ RTL/main_top.v
@@ -1,6 +1,9 @@
 `timescale 1ns / 1ps
 
 module main_top(
-    output HSYNC,    // A screen begins a new line when it receives a horizontal sync
-    output VSYNC     // and a new frame on a vertical sync
+    input C27M,     // input clock (27 MHz)
+    output HSYNC,   // A screen begins a new line when it receives a horizontal sync
+    output VSYNC    // and a new frame on a vertical sync
 );
+
+endmodule
