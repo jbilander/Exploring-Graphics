10dddda4a1f1719e26bbd18ba0721e223eae9681
jbilander <jorgen.bilander@gmail.com>
2023-09-30 18:36:15 +0200

    Update file, example working

--- RTL/main_top.v
+++ RTL/main_top.v
@@ -7,18 +7,15 @@
 `timescale 1ns / 1ps
 
 module main_top(
-    input C27M,         // Input XTAL clock (27 MHz)
-    input RESET_n,      // Push-button S2 on Tang9k, press down to reset
-    output PCLK,        // Pixel-clock output
-    output HSYNC,       // A screen begins a new line when it receives a horizontal sync,
-    output VSYNC,       //  and a new frame on a vertical sync
-    output DE,          // Data enable. This signal is high when input pixel data is valid to the transmitter 
-                        //  and low otherwise. It is critical that this signal have the same
-                        //  setup/hold timing as the data bus.
-    output [4:0] RED,   // 5-bit DVI red
-    output [5:0] GREEN, // 6-bit DVI green
-    output [4:0] BLUE   // 5-bit DVI blue
-//    output reg LED
+    input C27M,             // Input XTAL clock (27 MHz)
+    input RESET_n,          // Push-button S2 on Tang9k, press down to reset
+    output PCLK,            // Pixel-clock output
+    output HSYNC,           // A screen begins a new line when it receives a horizontal sync,
+    output VSYNC,           //  and a new frame on a vertical sync
+    output DE,              // Data enable. This signal is high when input pixel data is valid to the transmitter and low otherwise.
+    output reg [4:0] RED,   // 5-bit DVI red
+    output reg [5:0] GREEN, // 6-bit DVI green
+    output reg [4:0] BLUE   // 5-bit DVI blue
 );
 
 wire plock;             // Pixel-clock lock, indicates when Gowin rPLL generates a stable pixel-clock
@@ -44,27 +41,23 @@ simple_480p display_inst (
     .DE(DE)
 );
 
-/*
-reg [31:0] counter;
+assign square = (sx > 220 && sx < 420) && (sy > 140 && sy < 340);
 
-always @ (negedge RESET_n or posedge PCLK) begin
+// paint colour: white inside square, blue outside
+wire [4:0] paint_r = square ? 5'hFF : 5'h1;
+wire [5:0] paint_g = square ? 6'hFF : 6'h3;
+wire [4:0] paint_b = square ? 5'hFF : 5'h5;
 
-    if (!RESET_n) begin
+// display colour: paint colour but black in blanking interval
+wire [4:0] display_r = DE ? paint_r : 5'h0;
+wire [5:0] display_g = DE ? paint_g : 6'h0;
+wire [4:0] display_b = DE ? paint_b : 5'h0;
 
-        LED <= 1'b0;
-        counter <= 32'b0;
-
-    end else begin
-
-        counter <= counter + 1'b1;
-
-        if (counter > 10000000) begin
-            LED <= ~LED;
-            counter <= 32'b0;
-        end
-    end
-
+//Pmod output
+always @(posedge PCLK) begin
+    RED <= display_r;
+    GREEN <= display_g;
+    BLUE <= display_b;
 end
-*/
 
 endmodule
