caa8b45bd973d1fcbe51540a0fc0e37d8f9c6a89
jbilander <jorgen.bilander@gmail.com>
2023-09-30 15:28:02 +0200

    Added simple_480p module

--- RTL/main_top.v
+++ RTL/main_top.v
@@ -1,3 +1,9 @@
+// Project F: FPGA Graphics - Simple 640x480p60 Display
+// (C)2023 Will Green, open source hardware released under the MIT License
+// Learn more at https://projectf.io/posts/fpga-graphics/
+//
+// Modified by J.Bilander for Tang Nano9K 2023-09-30
+
 `timescale 1ns / 1ps
 
 module main_top(
@@ -6,15 +12,32 @@ module main_top(
     output PCLK,        // Pixel-clock output
     output PLOCK,       // Pixel-clock lock, indicates when Gowin rPLL generates a stable pixel-clock
     output HSYNC,       // A screen begins a new line when it receives a horizontal sync,
-    output VSYNC,       // and a new frame on a vertical sync
+    output VSYNC,       //  and a new frame on a vertical sync
+    output DE,          // Data enable. This signal is high when input pixel data is valid to the transmitter 
+                        //  and low otherwise. It is critical that this signal have the same
+                        //  setup/hold timing as the data bus.
     output reg LED
 );
 
+// generate pixel clock
 clock_480p pclk_480p(
     .C27M(C27M),
     .RESET_n(RESET_n),
     .PCLK(PCLK),        // The rPLL generated 25.2 MHz pixel clock for 480p@60 Hz
     .PLOCK(PLOCK)
+);
+
+localparam CORDW = 10;      // screen coordinate width in bits
+wire [CORDW-1:0] sx, sy;    // screen coordinates
+
+simple_480p display_inst (
+    .PCLK(PCLK),
+    .RST_PCLK(!PLOCK),   // wait for clock lock
+    .SX(sx),
+    .SY(sy),
+    .HSYNC(HSYNC),
+    .VSYNC(VSYNC),
+    .DE(DE)
 );
 
 
