82aa13e2704afa1d98dd5307fbf77c3f16c31b79
jbilander <jorgen.bilander@gmail.com>
2023-09-30 12:05:52 +0200

    Modularize 480p pixel-clock generation

--- RTL/main_top.v
+++ RTL/main_top.v
@@ -1,27 +1,24 @@
 `timescale 1ns / 1ps
 
 module main_top(
-    input C27M,     // Input XTAL clock (27 MHz)
-    input RESET_n,  // Push-button S2 on Tang9k, press down to reset
-    output HSYNC,   // A screen begins a new line when it receives a horizontal sync,
-    output VSYNC,   // and a new frame on a vertical sync
+    input C27M,         // Input XTAL clock (27 MHz)
+    input RESET_n,      // Push-button S2 on Tang9k, press down to reset
+    output PCLK,        // Pixel-clock output
+    output HSYNC,       // A screen begins a new line when it receives a horizontal sync,
+    output VSYNC,       // and a new frame on a vertical sync
     output reg LED
-
 );
 
-wire C25M;      // The 25.2 MHz pixel clock for 480p@60 Hz
-wire C302M;     // The CLKOUT_FREQ=302.4 MHz gets divided by 12 to generate 25.2 MHz above
-
-Gowin_rPLL_25M gen_C27M(
-    .clkout(C302M), //output clkout
-    .clkoutd(C25M), //output clkoutd
-    .reset(!RESET_n), //input reset
-    .clkin(C27M) //input clkin
+clock_480p pclk_480p(
+    .C27M(C27M),
+    .RESET_n(RESET_n),
+    .PCLK(PCLK)         // The rPLL generated 25.2 MHz pixel clock for 480p@60 Hz
 );
+
 
 reg [31:0] counter;
 
-always @ (negedge RESET_n or posedge C25M) begin
+always @ (negedge RESET_n or posedge PCLK) begin
 
     if (!RESET_n) begin
 
