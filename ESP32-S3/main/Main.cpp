0159089fd27ce45412b52c8e869309ba892f1aea
jbilander <jorgen.bilander@gmail.com>
2023-11-19 10:56:39 +0100

    Blink external LED on GPIO47 test

--- ESP32-S3/main/Main.cpp
+++ ESP32-S3/main/Main.cpp
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+static uint8_t s_led_state = 0;
+
+static void configure_led(void)
+{
+    gpio_reset_pin(GPIO_NUM_47);
+    gpio_set_direction(GPIO_NUM_47, GPIO_MODE_OUTPUT);
+}
 
 extern "C" void app_main()
 {
-    printf("Hello world!\n");
+    configure_led();
+
+    while (1)
+    {
+        gpio_set_level(GPIO_NUM_47, s_led_state);
+        s_led_state = !s_led_state;
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
